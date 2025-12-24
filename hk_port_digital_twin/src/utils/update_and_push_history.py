089f4cb179ed06620f8b708c5a686e0e850930af
Bhavesh R <bapuis@gmail.com>
2025-12-24 22:28:54 +0800

    update raw data

--- hk_port_digital_twin/src/utils/update_and_push_history.py
+++ hk_port_digital_twin/src/utils/update_and_push_history.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 from hk_port_digital_twin.src.utils.process_history import HistoricalVesselDataProcessor
 
-# Configure logging
+# Configure loggings
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
